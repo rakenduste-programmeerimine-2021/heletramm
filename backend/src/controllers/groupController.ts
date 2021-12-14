fcbe327d2b4cbfb246493ca87865f64cde553b35
MaitJ <MaitJ@tlu.ee>
2021-12-14 20:29:20 +0200

    Closes #88, and added a test for adding to group

--- backend/src/controllers/groupController.ts
+++ backend/src/controllers/groupController.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Response, Request } from "express";
 import { getConnection, In } from "typeorm";
 import { Room, RoomType } from "../model/Room";
+import { User } from "../model/User";
+import { NotAuthorizedError } from "../error_handling/authErrors";
 
 
 export const CreateGroup = async (req: Request, res: Response, next: NextFunction) => {
@@ -42,6 +44,26 @@ export const MyGroups = async (req: Request, res: Response, next: NextFunction) 
     return res.status(200).json(rooms);
 }
 
-export const AddToGroup = async (req: Request, res: Response, next: NextFunction) => {
-    return
+export const AddToGroup = async (req: Request<{}, {}, {room_id: number, user_ids: number[]}>, res: Response, next: NextFunction) => {
+    const {room_id, user_ids} = req.body;
+
+    const connection = getConnection();
+    const roomRepository = connection.getRepository(Room);
+    const userRepository = connection.getRepository(User);
+
+    const room = await roomRepository.findOne({
+        relations: ['users'],
+        where: {id: room_id}
+    });
+
+    const userIds = room.users.map((user) => user.id);
+    if (!userIds.includes(req.user.id)) next(new NotAuthorizedError());
+
+    const newUsers = await userRepository.findByIds(user_ids);
+
+    room.users.push(...newUsers);
+
+    await roomRepository.save(room);
+
+    return res.status(200).json({message: "User added to group"});
 }
